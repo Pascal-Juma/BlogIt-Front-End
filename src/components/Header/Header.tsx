2bff7846f3a70d0ca47bd185602cd8e23871dec8
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-16 11:34:51 +0300

    update the header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,19 @@
 import Button, { ButtonProps} from '@mui/material/Button';
 import { styled }from '@mui/material/styles';
+import { Link } from "react-router-dom"
 import { blue } from '@mui/material/colors';
 import { FaLongArrowAltRight } from "react-icons/fa";
 import Navigation from '../Navigation/Navigation';
 import './Header.css'
 import logo from '../../assets/logo.png'
 
-const HeaderCta = styled(Button)<ButtonProps>(({ theme }) => ({
+const HeaderCta = styled(Button)<ButtonProps & { to: string }>(({ theme }) => ({
   fontSize: '1.5rem',
   fontFamily: 'var(--primary-font)',
   fontWeight: 400,
   color: theme.palette.getContrastText(blue[500]),
   backgroundColor: blue[500],
-  '&hover': {
+  '&:hover': {
     backgroundColor: blue[700],
   }
 }));
@@ -38,7 +39,7 @@ function Header() {
               <Navigation to={navLink.to}  label={navLink.label} />)
             }
             </ul>
-            <HeaderCta variant="contained"  component="a" href="/signup" sx={{ textTransform:  'capitalize'}}>Get Started <FaLongArrowAltRight />
+            <HeaderCta variant="contained"  component={Link} to='/signup' sx={{ textTransform:  'capitalize'}}>Get Started <FaLongArrowAltRight />
             </HeaderCta>
         </div>
       </nav>
