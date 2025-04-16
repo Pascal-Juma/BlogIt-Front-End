7369d40e076c18a00429a3eaa910d4779214f3e7
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-16 18:35:25 +0300

    update the blogit frontend

--- src/pages/Published/Published.tsx
+++ src/pages/Published/Published.tsx
@@ -70,7 +70,7 @@ function Published() {
           marginInline: 5,
         }}
       >
-        <Card >
+        <Card sx={{ p: 3, display: "flex", alignItems: "center", justifyContent: "center"}}>
         <div className="markdown-helper">
           <Typography  variant="h1" color="var(--color-red)" sx={{fontSize: 30, fontWeight: 400}} gutterBottom >
             {data && data.title}
